cff6d62f1040b1e6bcdb2691ce365878a8006dda
Nandor Devai <mail@nandordevai.net>
2021-04-10 16:33:12 +0200

    add controls for seed & elevation

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
 const sketch = new p5((p) => {
     const size = 25;
     const rowHeight = Math.cos(Math.PI / 6) * size;
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    const rows = Math.floor(height / (rowHeight * 2) - 2);
-    const columns = Math.floor(width / (size * 1.6));
+    const width = 2560;
+    const height = 1440;
+    const rows = 30;
+    const columns = 60;
     const hexes = [];
+    let elevation; // TODO: find better name
+    let seed;
 
     p.setup = () => {
-        p.randomSeed(setSeed());
+        p.noLoop();
+        loadValues();
+        p.randomSeed(seed);
         p.createCanvas(width, height);
         p.colorMode(p.HSB, 360, 100, 100);
         for (let y = 0; y < rows; y++) {
@@ -18,41 +22,80 @@ const sketch = new p5((p) => {
             }
             hexes.push(row);
         }
+        setHandlers();
     };
 
     p.draw = () => {
-        p.noLoop();
+        hexes.forEach(row => row.forEach(hex => hex.elevation = 0));
+        p.background(0, 0, 100, 1);
         let x = Math.floor(randint(columns / 2) + columns / 4) - 1;
         let y = Math.floor(randint(rows / 2) + rows / 4) - 1;
-        for (let i = 0; i < 14000; i++) {
+        for (let i = 0; i < elevation; i++) {
             x += randint(2) - 1;
-            if (x >= columns || x < 1) x = Math.floor(randint(columns / 2) + columns / 4) - 1;
+            if (x >= columns - 1 || x < 1) {
+                x = Math.floor(randint(columns / 2) + columns / 4) - 1;
+            }
             y += randint(2) - 1;
-            if (y >= rows || y < 1) y = Math.floor(randint(rows / 2) + rows / 4) - 1;
+            if (y >= rows - 1 || y < 1) {
+                y = Math.floor(randint(rows / 2) + rows / 4) - 1;
+            }
             const h = hexes[y][x];
             h.raise();
         }
         hexes.forEach(row => row.forEach(hex => hex.draw()));
     };
 
+    loadValues = () => {
+        elevation = getStoredValue('hexgen-elevation', 15000);
+        document.querySelector('#elevation-label').innerHTML = elevation;
+        seed = getStoredValue('hexgen-seed', randint(10000));
+        document.querySelector('#seed').value = seed;
+    };
+
+    setHandlers = () => {
+        document.querySelector('#elevation').addEventListener('input', (e) => {
+            elevation = e.target.value;
+            document.querySelector('#elevation-label').innerHTML = e.target.value;
+            localStorage.setItem('hexgen-elevation', e.target.value);
+            p.redraw();
+        });
+
+        document.querySelector('#regenerate-seed').addEventListener('click', () => {
+            const newSeed = randint(10000);
+            localStorage.setItem('hexgen-seed', newSeed);
+            document.querySelector('#seed').value = newSeed;
+            p.redraw();
+        });
+    };
+
     randint = (i) => {
         return Math.floor(p.random(i + 1));
     };
 
+    getStoredValue = (key, defaultValue) => {
+        let v = localStorage.getItem(key);
+        if (v === null) {
+            v = defaultValue;
+            localStorage.setItem(key, defaultValue);
+        }
+        return v;
+    };
+
+    // TODO: use getStoredValue instead
     setSeed = () => {
-        let seed = localStorage.getItem('hexgenSeed');
+        let seed = localStorage.getItem('hexgen-seed');
         if (seed === null) {
             seed = randint(10000);
-            localStorage.setItem('hexgenSeed', seed);
+            localStorage.setItem('hexgen-seed', seed);
         }
         return seed;
     };
 
     class Hex {
-        constructor(x, y, altitude = 0) {
+        constructor(x, y, elevation = 0) {
             this.x = x;
             this.y = y;
-            this.altitude = altitude;
+            this.elevation = elevation;
             this.setFillColor();
         }
 
@@ -64,12 +107,12 @@ const sketch = new p5((p) => {
                 mountains: p.color(30, 40, 35),
                 highMountains: p.color(0, 0, 100),
             };
-            this.color = colors[Object.keys(colors)[Math.floor(this.altitude)]];
+            this.color = colors[Object.keys(colors)[Math.floor(this.elevation)]];
         }
 
         raise() {
-            if (this.altitude < 4) {
-                this.altitude += 0.1;
+            if (this.elevation < 4) {
+                this.elevation += 0.1;
                 this.setFillColor();
             }
         }
@@ -92,7 +135,7 @@ const sketch = new p5((p) => {
             }
             p.endShape(p.CLOSE);
             p.noStroke();
-            if (this.altitude === 3) {
+            if (this.elevation === 3) {
                 p.fill(0, 0, 100);
             } else {
                 p.fill(0, 0, 0);
